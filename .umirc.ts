96735b901c407d9d8c72b80989605e5b79c291d1
bubucuo <276185876@qq.com>
2022-08-08 10:52:16 +0800

    Revert "init"
    This reverts commit dd05950fd009cd07858231f4778623d662a7a3da.

--- .umirc.ts
+++ .umirc.ts
@@ -1,23 +1,3 @@
 export default {
-  npmClient: "pnpm",
-  apiRoute: {
-    platform: "vercel",
-  },
-
-  // routes: [
-  //   {
-  //     path: "/",
-  //     component: "@/layouts/index",
-  routes: [
-    {exact: true, path: "/", component: "index"},
-    {exact: true, path: "/posts/create", component: "posts/create"},
-    {exact: true, path: "/login", component: "login"},
-    {exact: true, path: "/register", component: "register"},
-    {exact: true, path: "/posts/:postId", component: "posts/post"},
-  ],
-  // },
-  // ],
-
-  plugins: [require.resolve("@umijs/plugins/dist/tailwindcss")],
-  tailwindcss: {},
+  npmClient: 'pnpm'
 };
